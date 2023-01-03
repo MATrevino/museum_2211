940905e3b3fa04135846b1f4dcc9700061fe3232
Meredith Trevino <meredithtrevino@gmail.com>
2023-01-03 09:38:18 -0700

    'adds admit and patrons methods for it 3'

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,13 @@
 class Museum
-  attr_reader :name, :exhibits, :recommend_exhibits
+  attr_reader :name, 
+              :exhibits, 
+              :recommend_exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit_name)
@@ -13,4 +17,27 @@ class Museum
   def recommend_exhibits(patron)
     patron.interests
   end
+
+  def admit(patron)
+    @patrons.push(patron)
+  end
+  # def patrons_by_exhibit_interest
+  #   #returns hash[:exhibit] = [array of patrons interests in that exhibit]
+  # end
+
+  # def ticket_lottery_contestants
+  #   #returns an array of patrons that do not have enough money but are interested in exhibit
+  # end
+
+  # def draw_lottery_winner
+
+  # end
+
+  # def announce_lottery_winner
+  #   #need stub
+  # end
+
+  # def draw_lottery_winner
+  #   #need stub
+  # end
 end
